4bac1b9fd1515b1feaf0fad3c3ae36c6f3bba856
DALIA <daalia_des@hotmail.com>
2013-04-08 13:46:55 -0500

    versión 1.2

--- js/acciones.js
+++ js/acciones.js
@@ -28,21 +28,29 @@ $(document).ready(function(e) {
 		   }); 
 		   
 		   $('#derecha').swiperight(function(){
-			   navigator.notification.alert("deslizo a la derecha",function(){},"practica 1","aceptar");
-		   });
-		   $('#izquierda').swipelef(function(){
-			   navigator.notification.confirm("¿Qué desea hacer?",function(option));{ 
-			   	switch(opcion){
-		                      case 1:
+			   navigator.notification.alert("deslizo a la derecha",function(){},"Práctica 1","aceptar");
+			});   
+			   
+	      $('#izquierda').swipeleft(function(){
+			   navigator.notification.confirm("¿Qué desea hacer?",function(opcion){
+				   switch(opcion){ 
+				           case 1:
 		                           navigator.notification.beep(1);
 		                      break;
-		                       
+
 		                      case 2:
 		                           navigator.notification.vibrate(500);
-			                   navigator.notification.vibrate(500);
+			                       navigator.notification.vibrate(500);
 		                       break;		   		   		   		   
 		                             }	//switch		   		
-		   } "Práctica 1", "beep, vibrate");
+		   }, "Práctica 1", "beep, vibrar, cancelar");
 		   });//funcion swipe
    },false);
+   
+   		   $('table td').eq(1).text(device.name);
+   		   $('table td').eq(3).text(device.cordova);
+   		   $('table td').eq(5).text(device.platform);
+   		   $('table td').eq(7).text(device.version);		   
+   		   $('table td').eq(9).text(device.UUID);		   
+		   
 });
